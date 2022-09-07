8e100920906d2fed1db79b981beafcd2136c295b
jieunlee <jieun3181@gmail.com>
2022-09-07 21:50:37 +0900

    0907

--- 교수님추천/탐색/2606.바이러스.py
+++ 교수님추천/탐색/2606.바이러스.py
@@ -1,6 +1,20 @@
 n = int(input())
 v = int(input())
-visited = []
-for i in range(v):
+gp = [[]*n for _ in range(n+1)]
+for _ in range(v):
     s, e = map(int, input().split())
-    visited.append()
+    gp[s].append(e)
+    gp[e].append(s)
+cnt = 0
+visited = [0]*(n+1)
+
+def dfs(start):
+    global cnt
+    visited[start] = 1
+    for i in gp[start]:
+        if visited[i] == 0:
+            dfs(i)
+            cnt += 1
+
+dfs(1)
+print(cnt)
